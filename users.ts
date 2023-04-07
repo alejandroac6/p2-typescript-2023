1097a9ce9d301b982f1ced52358577d22ef9b9ab
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-04-07 18:11:04 +0100

    Jugando con la base de datos

--- users.ts
+++ users.ts
@@ -31,12 +31,54 @@ export class User {
   }
 }
 
+const range= (n)=>{
+
+  let i = 0
+  let list=[]
+  while (i<n) {
+    i++
+    list.push(i)
+    
+  }
+
+  return list
+}
+
+export const loadCharacters = async(n:number)=>{
+
+  const list=range(n)
+  const url=`https://rickandmortyapi.com/api/character/${list}`
+
+
+  const response=await fetch(url);
+  const Characters=await response.json()
+  console.log(Characters)
+
+
+
+
+
+
+
+  
+
+
+}
+
 export const loadUsers = async (n: number) => {
   const response = await fetch(`https://randomuser.me/api?results=${n}`);
+
   const { results } = (await response.json()) as { results: any[] };
+  console.log(results)
   const users: Array<User> = [];
   for (const { gender, name, location, login, email, picture } of results) {
     users.push(new User(gender, name, location, login, email, picture));
   }
+
   return users;
 };
+
+// loadUsers(3)
+loadCharacters(5)
+
+// console.log(range(5))
