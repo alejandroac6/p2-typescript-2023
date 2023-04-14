b1e50e260cba409a0aba262204926c8e4b08be0d
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-04-14 22:14:22 +0100

    Cleaning Pau's remaining code

--- users.ts
+++ users.ts
@@ -20,37 +20,6 @@ export class Character{
   ){}
 }
 
-export class User {
-  constructor(
-    public gender: "male" | "female",
-    public name: {
-      title: "Mr" | "Mrs";
-      first: string;
-      last: string;
-    },
-    public location: {
-      street: string;
-      city: string;
-      state: string;
-      country: string;
-      postcode: number;
-    },
-    public login: {
-      username: string;
-      password: string;
-    },
-    public email: string,
-    public picture: {
-      large: string;
-      medium: string;
-      thumbnail: string;
-    }
-  ) {}
-
-  get fullName() {
-    return `${this.name.first} ${this.name.last}`;
-  }
-}
 
 const range= (n:number)=>{
   let i = 0
@@ -108,15 +77,3 @@ export const loadCharacters = async(n:number)=>{
 
   return world_Characters
 }
-
-export const loadUsers = async (n: number) => {
-  const response = await fetch(`https://randomuser.me/api?results=${n}`);
-
-  const { results } = (await response.json()) as { results: any[] };
-  const users: Array<User> = [];
-  for (const { gender, name, location, login, email, picture } of results) {
-    users.push(new User(gender, name, location, login, email, picture));
-  }
-
-  return users;
-};
